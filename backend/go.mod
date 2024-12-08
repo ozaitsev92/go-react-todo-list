86ac7befe0d4958ae79c90f48a29ecdacef51d7e
ozaitsev92 <ozaitsev92@gmail.com>
2024-12-08 11:40:31 +0200

    Add unit tests for UserUseCase

--- backend/go.mod
+++ backend/go.mod
@@ -5,10 +5,13 @@ go 1.23.3
 require (
 	github.com/BurntSushi/toml v1.4.0
 	github.com/gin-gonic/gin v1.10.0
+	github.com/golang-jwt/jwt v3.2.2+incompatible
 	github.com/google/uuid v1.6.0
+	github.com/ory/dockertest/v3 v3.11.0
 	github.com/prometheus/client_golang v1.20.5
 	github.com/rs/zerolog v1.33.0
 	go.mongodb.org/mongo-driver v1.17.0
+	golang.org/x/crypto v0.27.0
 	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22
 )
 
@@ -37,7 +40,6 @@ require (
 	github.com/go-viper/mapstructure/v2 v2.2.1 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
-	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/golang/snappy v0.0.4 // indirect
 	github.com/google/shlex v0.0.0-20191202100458-e7afc7fbc510 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
@@ -47,7 +49,6 @@ require (
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
-	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/moby/docker-image-spec v1.3.1 // indirect
 	github.com/moby/sys/user v0.3.0 // indirect
 	github.com/moby/term v0.5.0 // indirect
@@ -58,7 +59,6 @@ require (
 	github.com/opencontainers/go-digest v1.0.0 // indirect
 	github.com/opencontainers/image-spec v1.1.0 // indirect
 	github.com/opencontainers/runc v1.2.2 // indirect
-	github.com/ory/dockertest/v3 v3.11.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/prometheus/client_model v0.6.1 // indirect
@@ -75,7 +75,6 @@ require (
 	github.com/xeipuuv/gojsonschema v1.2.0 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20240726163527-a2c0da244d78 // indirect
 	golang.org/x/arch v0.8.0 // indirect
-	golang.org/x/crypto v0.27.0 // indirect
 	golang.org/x/net v0.26.0 // indirect
 	golang.org/x/sync v0.8.0 // indirect
 	golang.org/x/sys v0.28.0 // indirect
--- backend/internal/usecase/user_application_service_test.go
+++ backend/internal/usecase/user_application_service_test.go
@@ -1,3 +1,204 @@
-package usecase
+package usecase_test
 
-//todo: implement
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"github.com/ozaitsev92/tododdd/config"
+	"github.com/ozaitsev92/tododdd/internal/domain/user"
+	repo "github.com/ozaitsev92/tododdd/internal/infrastructure/repository/user/memory"
+	"github.com/ozaitsev92/tododdd/internal/usecase"
+)
+
+func TestRegisterNewUser(t *testing.T) {
+	type args struct {
+		email    string
+		password string
+	}
+
+	type testCase struct {
+		name    string
+		args    args
+		want    user.User
+		wantErr error
+	}
+
+	tests := []testCase{
+		{
+			name: "Success",
+			args: args{
+				email:    "test@example.com",
+				password: "TestPassword1",
+			},
+			want: user.User{
+				Email:    "test@example.com",
+				Password: "TestPassword1",
+			},
+			wantErr: nil,
+		},
+		{
+			name: "Empty email",
+			args: args{
+				email:    "",
+				password: "TestPassword1",
+			},
+			want:    user.User{},
+			wantErr: user.ErrInvalidEmail,
+		},
+		{
+			name: "Empty password",
+			args: args{
+				email:    "test@example.com",
+				password: "",
+			},
+			want:    user.User{},
+			wantErr: user.ErrInvalidPassword,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			repo := repo.NewRepository(config.Config{})
+
+			s := usecase.NewUserUseCase(repo)
+			newUser, err := s.RegisterNewUser(context.Background(), tt.args.email, tt.args.password)
+
+			if !errors.Is(err, tt.wantErr) {
+				t.Errorf("s.RegisterNewUser() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if err == nil && newUser.Email != tt.args.email {
+				t.Errorf("s.RegisterNewUser() Email = %v, want %v", newUser.Email, tt.want.Email)
+			}
+			if err == nil && !newUser.ComparePassword(tt.want.Password) {
+				t.Errorf("s.RegisterNewUser() Completed = %v, want %v", newUser.Password, tt.want.Password)
+			}
+			if err == nil && newUser.UpdatedAt.IsZero() {
+				t.Error("s.RegisterNewUser() UpdatedAt is zero")
+			}
+		})
+	}
+}
+
+func TestGetUserByID(t *testing.T) {
+	type args struct {
+		email    string
+		password string
+	}
+
+	type testCase struct {
+		name    string
+		args    args
+		want    user.User
+		wantErr error
+	}
+
+	tests := []testCase{
+		{
+			name: "Success",
+			args: args{
+				email:    "test@example.com",
+				password: "TestPassword1",
+			},
+			want: user.User{
+				Email:    "test@example.com",
+				Password: "TestPassword1",
+			},
+			wantErr: nil,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			newUser, err := user.NewUser(tt.args.email, tt.args.password)
+			if !errors.Is(err, tt.wantErr) {
+				t.Errorf("s.RegisterNewUser() error = %v, wantErr %v", err, tt.wantErr)
+			}
+
+			repo := repo.NewRepository(config.Config{})
+			err = repo.Save(context.Background(), newUser)
+			if !errors.Is(err, tt.wantErr) {
+				t.Errorf("s.RegisterNewUser() error = %v, wantErr %v", err, tt.wantErr)
+			}
+
+			s := usecase.NewUserUseCase(repo)
+			foundUser, err := s.GetUserByID(context.Background(), newUser.ID)
+
+			if !errors.Is(err, tt.wantErr) {
+				t.Errorf("s.RegisterNewUser() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if err == nil && foundUser.Email != tt.args.email {
+				t.Errorf("s.RegisterNewUser() Email = %v, want %v", newUser.Email, tt.want.Email)
+			}
+			if err == nil && !foundUser.ComparePassword(tt.want.Password) {
+				t.Errorf("s.RegisterNewUser() Completed = %v, want %v", newUser.Password, tt.want.Password)
+			}
+			if err == nil && foundUser.UpdatedAt.IsZero() {
+				t.Error("s.RegisterNewUser() UpdatedAt is zero")
+			}
+		})
+	}
+}
+
+func TestGetUserByEmail(t *testing.T) {
+	type args struct {
+		email    string
+		password string
+	}
+
+	type testCase struct {
+		name    string
+		args    args
+		want    user.User
+		wantErr error
+	}
+
+	tests := []testCase{
+		{
+			name: "Success",
+			args: args{
+				email:    "test@example.com",
+				password: "TestPassword1",
+			},
+			want: user.User{
+				Email:    "test@example.com",
+				Password: "TestPassword1",
+			},
+			wantErr: nil,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			newUser, err := user.NewUser(tt.args.email, tt.args.password)
+			if !errors.Is(err, tt.wantErr) {
+				t.Errorf("s.RegisterNewUser() error = %v, wantErr %v", err, tt.wantErr)
+			}
+
+			repo := repo.NewRepository(config.Config{})
+			err = repo.Save(context.Background(), newUser)
+			if !errors.Is(err, tt.wantErr) {
+				t.Errorf("s.RegisterNewUser() error = %v, wantErr %v", err, tt.wantErr)
+			}
+
+			s := usecase.NewUserUseCase(repo)
+			foundUser, err := s.GetUserByEmail(context.Background(), newUser.Email)
+
+			if !errors.Is(err, tt.wantErr) {
+				t.Errorf("s.RegisterNewUser() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if err == nil && foundUser.Email != tt.args.email {
+				t.Errorf("s.RegisterNewUser() Email = %v, want %v", newUser.Email, tt.want.Email)
+			}
+			if err == nil && !foundUser.ComparePassword(tt.want.Password) {
+				t.Errorf("s.RegisterNewUser() Completed = %v, want %v", newUser.Password, tt.want.Password)
+			}
+			if err == nil && foundUser.UpdatedAt.IsZero() {
+				t.Error("s.RegisterNewUser() UpdatedAt is zero")
+			}
+		})
+	}
+}
