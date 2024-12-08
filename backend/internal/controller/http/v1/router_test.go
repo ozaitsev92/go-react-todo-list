77ab743482f03871bf52bc92355b1e6fcea86501
ozaitsev92 <ozaitsev92@gmail.com>
2024-12-08 13:51:46 +0200

    Add integration tests for "/v1/tasks/*" routes

--- backend/internal/controller/http/v1/router_test.go
+++ backend/internal/controller/http/v1/router_test.go
@@ -18,13 +18,15 @@ import (
 	v1 "github.com/ozaitsev92/tododdd/internal/controller/http/v1"
 	"github.com/ozaitsev92/tododdd/internal/controller/http/v1/jwt"
 	"github.com/ozaitsev92/tododdd/internal/controller/http/v1/model"
+	"github.com/ozaitsev92/tododdd/internal/domain/task"
 	"github.com/ozaitsev92/tododdd/internal/domain/user"
 	"github.com/ozaitsev92/tododdd/internal/usecase"
 	"github.com/ozaitsev92/tododdd/pkg/mongodb"
 
 	taskRepository "github.com/ozaitsev92/tododdd/internal/infrastructure/repository/task/mongo"
+	taskConverter "github.com/ozaitsev92/tododdd/internal/infrastructure/repository/task/mongo/converter"
 	userRepository "github.com/ozaitsev92/tododdd/internal/infrastructure/repository/user/mongo"
-	"github.com/ozaitsev92/tododdd/internal/infrastructure/repository/user/mongo/converter"
+	userConverter "github.com/ozaitsev92/tododdd/internal/infrastructure/repository/user/mongo/converter"
 )
 
 var (
@@ -186,7 +188,7 @@ func TestRepositoryLogin(t *testing.T) {
 
 	// Add the user to the users collection
 	collection := mongodb.NewOrGetSingleton(cfg).Collection("users")
-	mongoUser := converter.ToRepoFromUser(u)
+	mongoUser := userConverter.ToRepoFromUser(u)
 	_, err = collection.InsertOne(context.Background(), mongoUser)
 	if err != nil {
 		t.Errorf("/v1/users/login failed to save a new user: err = '%v'", err)
@@ -237,15 +239,14 @@ func TestRepositoryLogin(t *testing.T) {
 func TestRepositoryLogout(t *testing.T) {
 	router, cfg, _ := setNewRouter()
 
-	rawPassword := "Password123"
-	u, err := user.NewUser("test1@example.com", rawPassword)
+	u, err := user.NewUser("test1@example.com", "Password123")
 	if err != nil {
 		t.Errorf("/v1/users/logout failed to create a new user: err = '%v'", err)
 	}
 
 	// Add the user to the users collection
 	collection := mongodb.NewOrGetSingleton(cfg).Collection("users")
-	mongoUser := converter.ToRepoFromUser(u)
+	mongoUser := userConverter.ToRepoFromUser(u)
 	_, err = collection.InsertOne(context.Background(), mongoUser)
 	if err != nil {
 		t.Errorf("/v1/users/logout failed to save a new user: err = '%v'", err)
@@ -292,15 +293,14 @@ func TestRepositoryLogout(t *testing.T) {
 func TestRepositoryCurrent(t *testing.T) {
 	router, cfg, jwtService := setNewRouter()
 
-	rawPassword := "Password123"
-	u, err := user.NewUser("test1@example.com", rawPassword)
+	u, err := user.NewUser("test1@example.com", "Password123")
 	if err != nil {
 		t.Errorf("/v1/users/current failed to create a new user: err = '%v'", err)
 	}
 
 	// Add the user to the users collection
 	collection := mongodb.NewOrGetSingleton(cfg).Collection("users")
-	mongoUser := converter.ToRepoFromUser(u)
+	mongoUser := userConverter.ToRepoFromUser(u)
 	_, err = collection.InsertOne(context.Background(), mongoUser)
 	if err != nil {
 		t.Errorf("/v1/users/current failed to save a new user: err = '%v'", err)
@@ -328,14 +328,321 @@ func TestRepositoryCurrent(t *testing.T) {
 
 	err = json.Unmarshal(w.Body.Bytes(), &response)
 	if err != nil {
-		t.Errorf("/v1/users error = '%v'", err)
+		t.Errorf("/v1/users/current error = '%v'", err)
 	}
 
 	if response.ID != u.ID.String() {
-		t.Errorf("/v1/users got = '%v', want = '%v'", response.ID, u.ID)
+		t.Errorf("/v1/users/current got = '%v', want = '%v'", response.ID, u.ID)
 	}
 
 	if response.Email != u.Email {
-		t.Errorf("/v1/users got = '%v', want = '%v'", response.Email, u.Email)
+		t.Errorf("/v1/users/current got = '%v', want = '%v'", response.Email, u.Email)
+	}
+}
+
+func TestRepositoryCreateTask(t *testing.T) {
+	router, cfg, jwtService := setNewRouter()
+
+	u, err := user.NewUser("test1@example.com", "Password123")
+	if err != nil {
+		t.Errorf("/v1/tasks failed to create a new user: err = '%v'", err)
+	}
+
+	// Add the user to the users collection
+	collection := mongodb.NewOrGetSingleton(cfg).Collection("users")
+	mongoUser := userConverter.ToRepoFromUser(u)
+	_, err = collection.InsertOne(context.Background(), mongoUser)
+	if err != nil {
+		t.Errorf("/v1/tasks failed to save a new user: err = '%v'", err)
+	}
+
+	token, err := jwtService.CreateJWTTokenForUser(u.ID)
+	if err != nil {
+		return
+	}
+
+	jwtCookie := jwtService.AuthCookie(token)
+
+	payload := map[string]string{
+		"text": "task text",
+	}
+	req := newJsonRequest("POST", "/v1/tasks", payload)
+	req.AddCookie(&http.Cookie{Name: jwtCookie.Name, Value: jwtCookie.Value})
+
+	w := httptest.NewRecorder()
+
+	router.ServeHTTP(w, req)
+
+	if w.Code != 200 {
+		t.Errorf("/v1/tasks got = '%v', want = '%v'", w.Code, 200)
+	}
+
+	var response model.Task
+
+	err = json.Unmarshal(w.Body.Bytes(), &response)
+	if err != nil {
+		t.Errorf("/v1/tasks error = '%v'", err)
+	}
+
+	if response.UserID != u.ID.String() {
+		t.Errorf("/v1/tasks got = '%v', want = '%v'", response.ID, u.ID)
+	}
+
+	if response.Text != payload["text"] {
+		t.Errorf("/v1/tasks got = '%v', want = '%v'", response.Text, payload["text"])
+	}
+
+	if response.Completed {
+		t.Error("/v1/tasks task should not be completed")
+	}
+}
+
+func TestRepositoryUpdateTask(t *testing.T) {
+	router, cfg, jwtService := setNewRouter()
+
+	// Add the user to the users collection
+	u, err := user.NewUser("test1@example.com", "Password123")
+	if err != nil {
+		t.Errorf("/v1/tasks/:id failed to create a new user: err = '%v'", err)
+	}
+
+	mongoUser := userConverter.ToRepoFromUser(u)
+	_, err = mongodb.NewOrGetSingleton(cfg).Collection("users").InsertOne(context.Background(), mongoUser)
+	if err != nil {
+		t.Errorf("/v1/tasks/:id failed to save a new user: err = '%v'", err)
+	}
+
+	// Add the task to the users collection
+	ti, err := task.NewTask("task text v1", u.ID)
+	if err != nil {
+		t.Errorf("/v1/tasks/:id failed to create a new user: err = '%v'", err)
+	}
+
+	mongoTask := taskConverter.ToRepoFromTask(ti)
+	_, err = mongodb.NewOrGetSingleton(cfg).Collection("tasks").InsertOne(context.Background(), mongoTask)
+	if err != nil {
+		t.Errorf("/v1/tasks/:id failed to save a new task: err = '%v'", err)
+	}
+
+	token, err := jwtService.CreateJWTTokenForUser(u.ID)
+	if err != nil {
+		return
+	}
+
+	jwtCookie := jwtService.AuthCookie(token)
+
+	payload := map[string]string{
+		"text": "task text v2",
+	}
+	req := newJsonRequest("PUT", "/v1/tasks/"+ti.ID.String(), payload)
+	req.AddCookie(&http.Cookie{Name: jwtCookie.Name, Value: jwtCookie.Value})
+
+	w := httptest.NewRecorder()
+
+	router.ServeHTTP(w, req)
+
+	if w.Code != 200 {
+		t.Errorf("/v1/tasks/:id got = '%v', want = '%v'", w.Code, 200)
+	}
+
+	var response model.Task
+
+	err = json.Unmarshal(w.Body.Bytes(), &response)
+	if err != nil {
+		t.Errorf("/v1/tasks/:id error = '%v'", err)
+	}
+
+	if response.UserID != u.ID.String() {
+		t.Errorf("/v1/tasks/:id got = '%v', want = '%v'", response.ID, u.ID)
+	}
+
+	if response.Text != payload["text"] {
+		t.Errorf("/v1/tasks/:id got = '%v', want = '%v'", response.Text, payload["text"])
+	}
+
+	if response.Completed {
+		t.Error("/v1/tasks/:id task should not be completed")
+	}
+}
+
+func TestRepositoryMarkTaskCompleted(t *testing.T) {
+	router, cfg, jwtService := setNewRouter()
+
+	// Add the user to the users collection
+	u, err := user.NewUser("test1@example.com", "Password123")
+	if err != nil {
+		t.Errorf("/v1/tasks/:id/mark-completed failed to create a new user: err = '%v'", err)
+	}
+
+	mongoUser := userConverter.ToRepoFromUser(u)
+	_, err = mongodb.NewOrGetSingleton(cfg).Collection("users").InsertOne(context.Background(), mongoUser)
+	if err != nil {
+		t.Errorf("/v1/tasks/:id/mark-completed failed to save a new user: err = '%v'", err)
+	}
+
+	// Add the task to the users collection
+	ti, err := task.NewTask("task text v1", u.ID)
+	if err != nil {
+		t.Errorf("/v1/tasks/:id/mark-completed failed to create a new user: err = '%v'", err)
+	}
+
+	mongoTask := taskConverter.ToRepoFromTask(ti)
+	_, err = mongodb.NewOrGetSingleton(cfg).Collection("tasks").InsertOne(context.Background(), mongoTask)
+	if err != nil {
+		t.Errorf("/v1/tasks/:id/mark-completed failed to save a new task: err = '%v'", err)
+	}
+
+	token, err := jwtService.CreateJWTTokenForUser(u.ID)
+	if err != nil {
+		return
+	}
+
+	jwtCookie := jwtService.AuthCookie(token)
+
+	req := newJsonRequest("PUT", "/v1/tasks/"+ti.ID.String()+"/mark-completed", nil)
+	req.AddCookie(&http.Cookie{Name: jwtCookie.Name, Value: jwtCookie.Value})
+
+	w := httptest.NewRecorder()
+
+	router.ServeHTTP(w, req)
+
+	if w.Code != 200 {
+		t.Errorf("/v1/tasks/:id/mark-completed got = '%v', want = '%v'", w.Code, 200)
+	}
+
+	var response model.Task
+
+	err = json.Unmarshal(w.Body.Bytes(), &response)
+	if err != nil {
+		t.Errorf("/v1/tasks/:id/mark-completed error = '%v'", err)
+	}
+
+	if response.UserID != u.ID.String() {
+		t.Errorf("/v1/tasks/:id/mark-completed got = '%v', want = '%v'", response.ID, u.ID)
+	}
+
+	if response.Text != ti.Text {
+		t.Errorf("/v1/tasks/:id/mark-completed got = '%v', want = '%v'", response.Text, ti.Text)
+	}
+
+	if !response.Completed {
+		t.Error("/v1/tasks/:id/mark-completed task should be completed")
+	}
+}
+
+func TestRepositoryMarkTaskNotCompleted(t *testing.T) {
+	router, cfg, jwtService := setNewRouter()
+
+	// Add the user to the users collection
+	u, err := user.NewUser("test1@example.com", "Password123")
+	if err != nil {
+		t.Errorf("/v1/tasks/:id/mark-not-completed failed to create a new user: err = '%v'", err)
+	}
+
+	mongoUser := userConverter.ToRepoFromUser(u)
+	_, err = mongodb.NewOrGetSingleton(cfg).Collection("users").InsertOne(context.Background(), mongoUser)
+	if err != nil {
+		t.Errorf("/v1/tasks/:id/mark-not-completed failed to save a new user: err = '%v'", err)
+	}
+
+	// Add the task to the users collection
+	ti, err := task.NewTask("task text v1", u.ID)
+	if err != nil {
+		t.Errorf("/v1/tasks/:id/mark-not-completed failed to create a new user: err = '%v'", err)
+	}
+
+	ti.Completed = true
+	mongoTask := taskConverter.ToRepoFromTask(ti)
+	_, err = mongodb.NewOrGetSingleton(cfg).Collection("tasks").InsertOne(context.Background(), mongoTask)
+	if err != nil {
+		t.Errorf("/v1/tasks/:id/mark-not-completed failed to save a new task: err = '%v'", err)
+	}
+
+	token, err := jwtService.CreateJWTTokenForUser(u.ID)
+	if err != nil {
+		return
+	}
+
+	jwtCookie := jwtService.AuthCookie(token)
+
+	req := newJsonRequest("PUT", "/v1/tasks/"+ti.ID.String()+"/mark-not-completed", nil)
+	req.AddCookie(&http.Cookie{Name: jwtCookie.Name, Value: jwtCookie.Value})
+
+	w := httptest.NewRecorder()
+
+	router.ServeHTTP(w, req)
+
+	if w.Code != 200 {
+		t.Errorf("/v1/tasks/:id/mark-not-completed got = '%v', want = '%v'", w.Code, 200)
+	}
+
+	var response model.Task
+
+	err = json.Unmarshal(w.Body.Bytes(), &response)
+	if err != nil {
+		t.Errorf("/v1/tasks/:id/mark-not-completed error = '%v'", err)
+	}
+
+	if response.UserID != u.ID.String() {
+		t.Errorf("/v1/tasks/:id/mark-not-completed got = '%v', want = '%v'", response.ID, u.ID)
+	}
+
+	if response.Text != ti.Text {
+		t.Errorf("/v1/tasks/:id/mark-not-completed got = '%v', want = '%v'", response.Text, ti.Text)
+	}
+
+	if response.Completed {
+		t.Error("/v1/tasks/:id/mark-not-completed task should not be completed")
+	}
+}
+
+func TestRepositoryDeleteTask(t *testing.T) {
+	router, cfg, jwtService := setNewRouter()
+
+	// Add the user to the users collection
+	u, err := user.NewUser("test1@example.com", "Password123")
+	if err != nil {
+		t.Errorf("/v1/tasks/:id failed to create a new user: err = '%v'", err)
+	}
+
+	mongoUser := userConverter.ToRepoFromUser(u)
+	_, err = mongodb.NewOrGetSingleton(cfg).Collection("users").InsertOne(context.Background(), mongoUser)
+	if err != nil {
+		t.Errorf("/v1/tasks/:id failed to save a new user: err = '%v'", err)
+	}
+
+	// Add the task to the users collection
+	ti, err := task.NewTask("task text v1", u.ID)
+	if err != nil {
+		t.Errorf("/v1/tasks/:id failed to create a new user: err = '%v'", err)
+	}
+
+	ti.Completed = true
+	mongoTask := taskConverter.ToRepoFromTask(ti)
+	_, err = mongodb.NewOrGetSingleton(cfg).Collection("tasks").InsertOne(context.Background(), mongoTask)
+	if err != nil {
+		t.Errorf("/v1/tasks/:id failed to save a new task: err = '%v'", err)
+	}
+
+	token, err := jwtService.CreateJWTTokenForUser(u.ID)
+	if err != nil {
+		return
+	}
+
+	jwtCookie := jwtService.AuthCookie(token)
+
+	req := newJsonRequest("DELETE", "/v1/tasks/"+ti.ID.String(), nil)
+	req.AddCookie(&http.Cookie{Name: jwtCookie.Name, Value: jwtCookie.Value})
+
+	w := httptest.NewRecorder()
+
+	router.ServeHTTP(w, req)
+
+	if w.Code != 200 {
+		t.Errorf("/v1/tasks/:id got = '%v', want = '%v'", w.Code, 200)
+	}
+
+	if len(w.Body.Bytes()) == 0 {
+		t.Errorf("/v1/tasks/:id response body should be empty, got = '%s'", w.Body.String())
 	}
 }
