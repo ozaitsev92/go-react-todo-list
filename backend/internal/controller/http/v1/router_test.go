f5ed31219719245d6045fcc8df65b335b3dee06b
ozaitsev92 <ozaitsev92@gmail.com>
2024-12-08 13:29:48 +0200

    Add intergation tests for "/v1/users/*" routes

--- backend/internal/controller/http/v1/router_test.go
+++ backend/internal/controller/http/v1/router_test.go
@@ -1,9 +1,13 @@
 package v1_test
 
 import (
+	"bytes"
+	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net"
+	"net/http"
 	"net/http/httptest"
 	"os"
 	"testing"
@@ -13,14 +17,22 @@ import (
 	"github.com/ozaitsev92/tododdd/config"
 	v1 "github.com/ozaitsev92/tododdd/internal/controller/http/v1"
 	"github.com/ozaitsev92/tododdd/internal/controller/http/v1/jwt"
+	"github.com/ozaitsev92/tododdd/internal/controller/http/v1/model"
+	"github.com/ozaitsev92/tododdd/internal/domain/user"
 	"github.com/ozaitsev92/tododdd/internal/usecase"
+	"github.com/ozaitsev92/tododdd/pkg/mongodb"
 
 	taskRepository "github.com/ozaitsev92/tododdd/internal/infrastructure/repository/task/mongo"
 	userRepository "github.com/ozaitsev92/tododdd/internal/infrastructure/repository/user/mongo"
+	"github.com/ozaitsev92/tododdd/internal/infrastructure/repository/user/mongo/converter"
 )
 
 var (
 	MONGODB_PORT = ""
+)
+
+const (
+	jwtCookieName = "jwt-token"
 )
 
 type mockLogger struct{}
@@ -31,10 +43,20 @@ func (l *mockLogger) Warn(message string, args ...interface{})       {}
 func (l *mockLogger) Error(message interface{}, args ...interface{}) {}
 func (l *mockLogger) Fatal(message interface{}, args ...interface{}) {}
 
-func setNewRouter() *gin.Engine {
+func newJsonRequest(method, url string, payload map[string]string) *http.Request {
+	jsonPayload, _ := json.Marshal(payload)
+
+	req := httptest.NewRequest(method, url, bytes.NewBuffer(jsonPayload))
+	req.Header.Set("Content-Type", "application/json")
+
+	return req
+}
+
+func setNewRouter() (*gin.Engine, config.Config, *jwt.JWTService) {
 	cfg := config.Config{}
 	cfg.MongoDBName = "todo_test"
 	cfg.MongoUrl = fmt.Sprintf("mongodb://localhost:%s", MONGODB_PORT)
+	cfg.JWTSessionLength = 30
 
 	l := new(mockLogger)
 
@@ -59,7 +81,7 @@ func setNewRouter() *gin.Engine {
 
 	v1.NewRouter(handler, cfg, l, jwtService, taskUseCase, userUseCase)
 
-	return handler
+	return handler, cfg, jwtService
 }
 
 func TestMain(m *testing.M) {
@@ -93,9 +115,9 @@ func TestMain(m *testing.M) {
 }
 
 func TestRepositoryHealthz(t *testing.T) {
-	router := setNewRouter()
+	router, _, _ := setNewRouter()
 
-	req := httptest.NewRequest("GET", "/healthz", nil)
+	req := newJsonRequest("GET", "/healthz", nil)
 
 	w := httptest.NewRecorder()
 
@@ -107,9 +129,9 @@ func TestRepositoryHealthz(t *testing.T) {
 }
 
 func TestRepositoryMetrics(t *testing.T) {
-	router := setNewRouter()
+	router, _, _ := setNewRouter()
 
-	req := httptest.NewRequest("GET", "/metrics", nil)
+	req := newJsonRequest("GET", "/metrics", nil)
 
 	w := httptest.NewRecorder()
 
@@ -117,5 +139,203 @@ func TestRepositoryMetrics(t *testing.T) {
 
 	if w.Code != 200 {
 		t.Errorf("/metrics got = '%v', want = '%v'", w.Code, 200)
+	}
+}
+
+func TestRepositoryCreateUser(t *testing.T) {
+	router, _, _ := setNewRouter()
+
+	payload := map[string]string{
+		"password": "Password123",
+		"email":    "john@example.com",
+	}
+	req := newJsonRequest("POST", "/v1/users", payload)
+
+	w := httptest.NewRecorder()
+
+	router.ServeHTTP(w, req)
+
+	if w.Code != 201 {
+		t.Errorf("/v1/users got = '%v', want = '%v'", w.Code, 201)
+	}
+
+	var response model.User
+
+	err := json.Unmarshal(w.Body.Bytes(), &response)
+	if err != nil {
+		t.Errorf("/v1/users error = '%v'", err)
+	}
+
+	if response.Email != payload["email"] {
+		t.Errorf("/v1/users got = '%v', want = '%v'", response.Email, payload["email"])
+	}
+
+	if response.ID == "" {
+		t.Error("/v1/users User.ID is empty")
+	}
+}
+
+func TestRepositoryLogin(t *testing.T) {
+	router, cfg, _ := setNewRouter()
+
+	rawPassword := "Password123"
+	u, err := user.NewUser("test1@example.com", rawPassword)
+	if err != nil {
+		t.Errorf("/v1/users/login failed to create a new user: err = '%v'", err)
+	}
+
+	// Add the user to the users collection
+	collection := mongodb.NewOrGetSingleton(cfg).Collection("users")
+	mongoUser := converter.ToRepoFromUser(u)
+	_, err = collection.InsertOne(context.Background(), mongoUser)
+	if err != nil {
+		t.Errorf("/v1/users/login failed to save a new user: err = '%v'", err)
+	}
+
+	payload := map[string]string{
+		"password": rawPassword,
+		"email":    u.Email,
+	}
+	req := newJsonRequest("POST", "/v1/users/login", payload)
+
+	w := httptest.NewRecorder()
+
+	router.ServeHTTP(w, req)
+
+	if w.Code != 200 {
+		t.Errorf("/v1/users/login got = '%v', want = '%v'", w.Code, 200)
+	}
+
+	if len(w.Body.Bytes()) == 0 {
+		t.Errorf("/v1/users/login response body should be empty, got = '%s'", w.Body.String())
+	}
+
+	resp := w.Result()
+	cookies := resp.Cookies()
+
+	var testCookie *http.Cookie
+	for _, cookie := range cookies {
+		if cookie.Name == jwtCookieName {
+			testCookie = cookie
+			break
+		}
+	}
+
+	if testCookie == nil {
+		t.Errorf("/v1/users/login response must have a '%s' cookie", jwtCookieName)
+	}
+
+	if testCookie != nil && testCookie.Value == "" {
+		t.Errorf("/v1/users/login '%s' cookie must have a Value, got = '%s'", jwtCookieName, testCookie.Value)
+	}
+
+	if testCookie != nil && testCookie.MaxAge <= 0 {
+		t.Errorf("/v1/users/login '%s' cookie must have a MaxAge > 0, got = '%d'", jwtCookieName, testCookie.MaxAge)
+	}
+}
+
+func TestRepositoryLogout(t *testing.T) {
+	router, cfg, _ := setNewRouter()
+
+	rawPassword := "Password123"
+	u, err := user.NewUser("test1@example.com", rawPassword)
+	if err != nil {
+		t.Errorf("/v1/users/logout failed to create a new user: err = '%v'", err)
+	}
+
+	// Add the user to the users collection
+	collection := mongodb.NewOrGetSingleton(cfg).Collection("users")
+	mongoUser := converter.ToRepoFromUser(u)
+	_, err = collection.InsertOne(context.Background(), mongoUser)
+	if err != nil {
+		t.Errorf("/v1/users/logout failed to save a new user: err = '%v'", err)
+	}
+
+	req := newJsonRequest("POST", "/v1/users/logout", nil)
+
+	w := httptest.NewRecorder()
+
+	router.ServeHTTP(w, req)
+
+	if w.Code != 200 {
+		t.Errorf("/v1/users/logout got = '%v', want = '%v'", w.Code, 200)
+	}
+
+	if len(w.Body.Bytes()) == 0 {
+		t.Errorf("/v1/users/logout response body should be empty, got = '%s'", w.Body.String())
+	}
+
+	resp := w.Result()
+	cookies := resp.Cookies()
+
+	var testCookie *http.Cookie
+	for _, cookie := range cookies {
+		if cookie.Name == jwtCookieName {
+			testCookie = cookie
+			break
+		}
+	}
+
+	if testCookie == nil {
+		t.Errorf("/v1/users/logout response must have a '%s' cookie", jwtCookieName)
+	}
+
+	if testCookie != nil && testCookie.Value != "" {
+		t.Errorf("/v1/users/logout '%s' cookie must not have a Value, got = '%s'", jwtCookieName, testCookie.Value)
+	}
+
+	if testCookie != nil && testCookie.MaxAge != -1 {
+		t.Errorf("/v1/users/logout '%s' cookie must have a MaxAge == -1, got = '%d'", jwtCookieName, testCookie.MaxAge)
+	}
+}
+
+func TestRepositoryCurrent(t *testing.T) {
+	router, cfg, jwtService := setNewRouter()
+
+	rawPassword := "Password123"
+	u, err := user.NewUser("test1@example.com", rawPassword)
+	if err != nil {
+		t.Errorf("/v1/users/current failed to create a new user: err = '%v'", err)
+	}
+
+	// Add the user to the users collection
+	collection := mongodb.NewOrGetSingleton(cfg).Collection("users")
+	mongoUser := converter.ToRepoFromUser(u)
+	_, err = collection.InsertOne(context.Background(), mongoUser)
+	if err != nil {
+		t.Errorf("/v1/users/current failed to save a new user: err = '%v'", err)
+	}
+
+	token, err := jwtService.CreateJWTTokenForUser(u.ID)
+	if err != nil {
+		return
+	}
+
+	jwtCookie := jwtService.AuthCookie(token)
+
+	req := newJsonRequest("GET", "/v1/users/current", nil)
+	req.AddCookie(&http.Cookie{Name: jwtCookie.Name, Value: jwtCookie.Value})
+
+	w := httptest.NewRecorder()
+
+	router.ServeHTTP(w, req)
+
+	if w.Code != 200 {
+		t.Errorf("/v1/users/current got = '%v', want = '%v'", w.Code, 200)
+	}
+
+	var response model.User
+
+	err = json.Unmarshal(w.Body.Bytes(), &response)
+	if err != nil {
+		t.Errorf("/v1/users error = '%v'", err)
+	}
+
+	if response.ID != u.ID.String() {
+		t.Errorf("/v1/users got = '%v', want = '%v'", response.ID, u.ID)
+	}
+
+	if response.Email != u.Email {
+		t.Errorf("/v1/users got = '%v', want = '%v'", response.Email, u.Email)
 	}
 }
